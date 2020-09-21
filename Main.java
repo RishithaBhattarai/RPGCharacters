d85bf162e129e729cc79cd7bf15f77790183830d
RishithaBhattarai <69611262+RishithaBhattarai@users.noreply.github.com>
2020-09-21 11:57:42 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main 
+{
+
+    public static void main(String[] args) {
+      Warrior c1 = new Warrior("Jim");
+      Warrior c2 = new Warrior("Bob");
+      Warrior c3 = c1;
+      Warrior c4 = new Warrior("Min", 5);
+      Warrior c4 = new Warrior("Jeff", 7);
+    } 
+} 
